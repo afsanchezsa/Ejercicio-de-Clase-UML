bdd1d1a069ffa9637534ce712e2bce58fd85b254
Estudiante <Estudiante@10.203.1.246>
2017-08-24 15:45:07 -0500

    Hola Mundo

---
+++ Ejercicio Clase UML/src/ejercicio/clase/uml/fecha.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio.clase.uml;
+
+/**
+ *
+ * @author Estudiante
+ */
+public class fecha {
+    int dia;
+    int mes;
+    int anio;
+    int hora;
+}
